ab2e7225e0e2954405f87b90831419573e83e540
Ricardo <w.deogratias@gmail.com>
2015-06-04 16:56:52 +0800

    stage work 6.4

--- deploy.bash
+++ deploy.bash
@@ -15,9 +15,9 @@ go get github.com/gorilla/schema
 go get gopkg.in/mgo.v2
 
 # beego
-go get github.com/astaxie.beego
-go get github.com/beggo/bee
+go get github.com/astaxie/beego
+go get github.com/beego/bee
 go get github.com/beego/i18n
 
 # tls
-go run $GOROOT/src/crypto/tls/generate_cert.go --host 127.0.0.1
+# go run $GOROOT/src/crypto/tls/generate_cert.go --host 127.0.0.1
