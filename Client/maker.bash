f50e2e1c77ee49045ba59fb808fbf8d448f58c3d
Ricardo <ricardo@liangchongsmini.ads.autodesk.com>
2015-06-29 12:41:58 +0800

    maker bash fix

--- Client/maker.bash
+++ Client/maker.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
+if [ -e build ];
+then
+	rm -r build
+fi
+
+if [ -e build-3rd ];
+then
+	rm -r build-3rd
+fi
+
 mkdir build
 cd build
-#cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.cmake ../3rd
 cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.cmake ../Squirrel
 cd ..
